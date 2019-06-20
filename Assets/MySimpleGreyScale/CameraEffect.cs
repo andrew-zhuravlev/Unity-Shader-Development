07d86dc8084dedd7a76e178626803fdf3c2dc176
Andrew Zhuravlev <frntxacc@gmail.com>
2019-06-21 00:15:24 +0300

    Added grayscale naruto to new scene. Created shader for image effect grayscale and custom shader which can be applied on sprite.

---
+++ Assets/MySimpleGreyScale/CameraEffect.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+
+public class CameraEffect : MonoBehaviour
+{
+    [SerializeField] Shader imageEffectShader;
+
+    Material imageEffect;
+
+    private void Awake() {
+        imageEffect = new Material(imageEffectShader);
+    }
+
+    private void OnRenderImage(RenderTexture source, RenderTexture destination) {
+        Graphics.Blit(source, destination, imageEffect);
+    }
+}
